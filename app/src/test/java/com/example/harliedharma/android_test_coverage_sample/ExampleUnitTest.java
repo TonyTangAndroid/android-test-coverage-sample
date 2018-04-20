48f5957e2b0d5715b0d18caf035c7230e8ad07be
harlie dharma <t-harlie.dharma@traveloka.com>
2018-04-20 18:32:35 +0700

    Add method test

--- app/src/test/java/com/example/harliedharma/android_test_coverage_sample/ExampleUnitTest.java
+++ app/src/test/java/com/example/harliedharma/android_test_coverage_sample/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void test_squareArea() throws Exception {
+        assertEquals( 100.0, new MainActivity().calculateSquareArea(10), 1e-7 );
+    }
 }
